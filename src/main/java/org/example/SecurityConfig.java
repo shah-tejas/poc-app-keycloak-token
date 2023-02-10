15fa8b2b03004789da8cbf91a2fceee9b2dc4214
Tejas Shah <tshah@appen.com>
2023-02-09 16:41:31 -0800

    remove unused imports

--- src/main/java/org/example/SecurityConfig.java
+++ src/main/java/org/example/SecurityConfig.java
@@ -1,22 +1,10 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.util.Converter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
